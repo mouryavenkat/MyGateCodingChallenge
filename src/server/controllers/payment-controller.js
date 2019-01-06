568195f43a391b42d5f658625acf0cb925ad6b51
mouryavenkat <codeforlyf@gmail.com>
2019-01-06 12:08:39 +0530

    firstcommit

--- src/server/controllers/payment-controller.js
+++
@@ -1,9 +0,0 @@
-
-// Starts executing the template received from request body
-
-const initialPayment = require('../../model/services/entityServices/payments/createInitialPayment');
-module.exports = (app) => {
-    // Group related api's
-    app.route('/initialPaymentSetup')
-        .post(initialPayment.AddPayment)
-}
