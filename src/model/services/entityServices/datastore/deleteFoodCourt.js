bd45aa520ca67a2d9ad02e4ff28b520ebd1146ff
root <root@oc8187121374.ibm.com>
2019-01-07 16:22:14 +0530

    readme update

--- src/model/services/entityServices/datastore/deleteFoodCourt.js
+++ src/model/services/entityServices/datastore/deleteFoodCourt.js
@@ -6,8 +6,8 @@ const deleteFoodCourt = async (req, res) => {
         console.log(req.params)
         logger.info(`Deleting the restauant named ${req.params['foodCourtId']}`);
         const modelForFoodCourt = FoodCourtModel.createModelForDataStore();
-        console.log({ applicant: req.params['foodCourtId'] })
-        const response = await deleteOperation.DeleteDocumentsByQuery(modelForFoodCourt, { applicant: req.params['foodCourtId'] });
+        console.log({ objectid: req.params['foodCourtId'] })
+        const response = await deleteOperation.DeleteDocumentsByQuery(modelForFoodCourt, { objectid: req.params['foodCourtId'] });
         if(response['n']==0){
             return res.status(404).json({code:404,message:`The restaurant ${req.params['foodCourtId']} isn't available to be deleted`})
         }
